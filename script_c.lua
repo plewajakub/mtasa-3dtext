cb35df39edd645872d73c56d98a3531a57884ec2
Jakub Plewa <70707081+plewajakub@users.noreply.github.com>
2023-03-06 01:53:18 +0100

    Update script_c.lua

--- script_c.lua
+++ script_c.lua
@@ -1,19 +1,131 @@
 setDevelopmentMode (true)
-
 local textObjects = {}
+local visibleTextObjects = {}
+local textid = 0
+
+-- tworzenie obiektu tekstu
+function createTextObject(posX, posY, posZ, content, color, font, distance) 
+    local txtObj = {
+        id = textid,
+        position = {posX, posY, posZ},
+        content = content,
+        color = color,
+        font = font,
+        rendering_distance = distance,
+        los_validation = false,
+        colSphere = nil
+    }
+    triggerServerEvent("onTextObjectCreate", resourceRoot, txtObj)
+end
+
+-- wykonanie update obiektu tekstowego
+-- przyklad uzycia: updateTextObject(1, {content="This is edited txtObj content"}) 
+function updateTextObject(id, data) -- data to obiekt zawierajacy opcjonalne argumenty, przesylamy tylko to co chcemy zmienic {posX, posY, posZ, content, color, font, distance}
+    id = tonumber(id)
+    for i,txtObj in ipairs(textObjects) do
+      if txtObj.id == id then
+        if data.posX and data.posY and data.posZ then
+          txtObj.position = {posX, posY, posZ}
+        end
+        
+        if data.content then
+          txtObj.content = data.content
+        end
+
+        if data.color then 
+          txtObj.color = data.color
+        end
+        
+        if data.font then 
+          txtObj.font = data.font
+        end
+
+        if data.distance then 
+          txtObj.rendering_distance = data.distance
+        end
+        setElementData(txtObj.colSphere, "txtObj", txtObj)
+        -- refresh w celu zaktualizowania tekstu, jezeli byl w danym momencie widoczny
+        onTextObjectLeave(txtObj)
+        onTextObjectEnter(txtObj)
+    end
+  end
+end
+addEvent("onTextUpdate", true)
+addEventHandler("onTextUpdate", resourceRoot, updateTextObject)
+
+-- request update obiektu tekstowego
+function reqTextUpdate(id, data)
+    triggerServerEvent("onTextUpdateRequest", resourceRoot, id, data)
+end    
+
+-- usuwanie obiektu tekstu
+function removeTextObject(ID)
+    for i, txtObj in ipairs(textObjects) do
+        if txtObj.id == tonumber(ID) then
+            destroyElement(txtObj.colSphere)
+            table.remove(textObjects, i)
+        end
+    end
+    for i, txtObj in ipairs(visibleTextObjects) do
+        if txtObj.id == tonumber(ID) then
+            destroyElement(txtObj.colSphere)
+            table.remove(visibleTextObjects, i)
+        end
+    end
+end    
+addEvent("onTextRemoval", true)
+addEventHandler("onTextRemoval", resourceRoot, removeTextObject)
+
+-- request usuniecia tekstu z serwera
+function reqTextDeletion(id)
+   triggerServerEvent("onTextDeletionRequest", resourceRoot, id) 
+end
+
+--request danych textobiektow z serwera
+function requestTextObjects()
+    triggerServerEvent("requestTextObjectsOnJoin", resourceRoot, localPlayer)
+end
+addEventHandler("onClientResourceStart", resourceRoot, requestTextObjects)
+
+
+-- odebranie wszystkich istniejacych obiektow tekstu na serwerze
+function getAllTextObjects(serverTextObjects)
+    textObjects = serverTextObjects
+    for i,txtObj in ipairs(textObjects) do
+        txtObj.colSphere = createColSphere(txtObj.position[1],txtObj.position[2],txtObj.position[3], txtObj.rendering_distance)
+        setElementData(txtObj.colSphere, "txtObj", txtObj)
+        addEventHandler("onClientColShapeHit", txtObj.colSphere, onColShapeHit)
+        addEventHandler("onClientColShapeLeave", txtObj.colSphere, onColShapeLeave)        
+    end    
+end
+addEvent("receiveFullTextObjectData", true)
+addEventHandler("receiveFullTextObjectData", resourceRoot, getAllTextObjects)
+
+
+-- odebranie pojedynczego obiektu tekstu od serwera
+function getSingleTextObject(txtObj)
+    txtObj.colSphere = createColSphere(txtObj.position[1],txtObj.position[2],txtObj.position[3], txtObj.rendering_distance)
+    setElementData(txtObj.colSphere, "txtObj", txtObj)
+    addEventHandler("onClientColShapeHit", txtObj.colSphere, onColShapeHit)
+    addEventHandler("onClientColShapeLeave", txtObj.colSphere, onColShapeLeave)
+    table.insert(textObjects, txtObj)
+end    
+addEvent("receiveTextObject", true)
+addEventHandler("receiveTextObject", resourceRoot, getSingleTextObject)
+
 
 -- wejscie do colsphere
 function onTextObjectEnter(txtObj)
-    table.insert(textObjects, txtObj) -- dodanie tekstu do 'widocznych'
+    table.insert(visibleTextObjects, txtObj) -- dodanie tekstu do 'widocznych'
 end
 addEvent("onTextObjectEnter", true)
 addEventHandler("onTextObjectEnter", resourceRoot, onTextObjectEnter)
 
 -- wyjscie z colsphere
 function onTextObjectLeave(txtObj)
-    for i, obj in ipairs(textObjects) do
+    for i, obj in ipairs(visibleTextObjects) do
         if obj.id == txtObj.id then
-            table.remove(textObjects, i) -- usuniecie tekstu z 'widocznych'
+            table.remove(visibleTextObjects, i) -- usuniecie tekstu z 'widocznych'
             break
         end
     end
@@ -21,13 +133,32 @@ end
 addEvent("onTextObjectLeave", true)
 addEventHandler("onTextObjectLeave", resourceRoot, onTextObjectLeave)
 
+-- Event handler kolizji gracza z colsphere
+function onColShapeHit(hitElement, matchingDimension)
+    if (hitElement == localPlayer) and matchingDimension then
+        local player = hitElement
+        local txtObj = getElementData(source, "txtObj") -- get the txtObj table from the colSphere
+        onTextObjectEnter(txtObj)
+    end
+end
+
+-- Event handler opuszczenia sphere przez gracza
+function onColShapeLeave(hitElement, matchingDimension)
+    if (hitElement == localPlayer) and matchingDimension then
+        local txtObj = getElementData(source, "txtObj")
+        onTextObjectLeave(txtObj)
+    end
+end
+
+
+
 -- rysowanie obiektow tekstu
 function drawTextObject(txtObj)
     local camX, camY, camZ = getCameraMatrix()
     local dist = getDistanceBetweenPoints3D(camX, camY, camZ, unpack(txtObj.position))
     -- walidacja line of sight
       txtX, txtY, txtZ = unpack(txtObj.position)
-      txtObj.los_validation = isLineOfSightClear(camX, camY, camZ, txtX, txtY, txtZ,false,false,false)
+      txtObj.los_validation = isLineOfSightClear(camX, camY, camZ, txtX, txtY, txtZ,true,true,false)
     if dist <= txtObj.rendering_distance and (txtObj.los_validation) then
     
         local alpha = 255
@@ -52,9 +183,8 @@ end
 
 -- draw loop rysujacy wszystkie obiekty tekstu mozliwie widoczne dla gracza
 function drawVisibleTextObjects()
-    for i, txtObj in ipairs(textObjects) do
+    for i, txtObj in ipairs(visibleTextObjects) do
         drawTextObject(txtObj)
-
     end
 end
 addEventHandler("onClientRender", root, drawVisibleTextObjects)
